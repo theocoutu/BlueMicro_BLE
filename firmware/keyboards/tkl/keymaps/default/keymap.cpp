0b90a4b773d4e59a7c8ec2479b0acb560ac4dea7
theocoutu <theodorec1998@gmail.com>
2023-03-05 23:51:11 -0600

    Update keymap.cpp

--- firmware/keyboards/tkl/keymaps/default/keymap.cpp
+++ firmware/keyboards/tkl/keymaps/default/keymap.cpp
@@ -1,20 +1,16 @@
 /*
 Copyright 2018-2021 <Pierre Constantineau>
-
 3-Clause BSD License
-
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
 LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 */
+
 #include "keymap.h"
 
 // Initialize matrix with nothing...
@@ -30,9 +26,7 @@ std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)};
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)};
       
       /*KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
@@ -51,20 +45,19 @@ std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
 
 void setupKeymap() {
 
-   uint32_t layer0_single[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+   uint32_t layer0_single[MATRIX_ROWS][MATRIX_COLS] = KEYMAP( \
         KC_F7,     KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_PSCREEN, KC_SCROLLLOCK, KC_PAUSE, \
         KC_8,      KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_INSERT,  KC_HOME,       KC_PGUP, \
         KC_I,      KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, KC_DELETE,  KC_END,        KC_PGDOWN, \
-        KC_K,      KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,  KC_NO,   KC_NO,      KC_NO,         KC_NO, \
-        KC_COMM,   KC_DOT,  KC_NO,   KC_SLSH,  KC_RSFT, KC_NO,   KC_NO,      KC_UP,         KC_NO, \
-        KC_NO,     KC_RALT, KC_APP,  KC_RCTRL, KC_NO,   KC_NO,   KC_LEFT,    KC_DOWN,       KC_RIGHT, \
-        KC_ESC,    KC_NO,   KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,      KC_F6,         KC_NO, \
-        KC_GRV,    KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,       KC_7,          KC_NO, \
-        KC_TAB,    KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,       KC_U,          KC_NO, \  
-        KC_CAPS,   KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,       KC_J,          KC_NO, \
-        KC_LSHIFT, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,       KC_M,          KC_NO, \
-        KC_LCTRL,  KC_LGUI, KC_LALT, KC_NO,    KC_NO,   KC_SPC,  KC_NO,      KC_NO,         KC_NO \
-    );
+        KC_K,      KC_L,    KC_SCLN, KC_QUOT,  KC_ENT, \
+        KC_COMM,   KC_DOT,           KC_SLSH,  KC_RSFT,                      KC_UP, \
+                   KC_RALT, KC_APP,  KC_RCTRL,                   KC_LEFT,    KC_DOWN,       KC_RIGHT, \
+        KC_ESC,             KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,      KC_F6, \
+        KC_GRV,    KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,       KC_7,  \
+        KC_TAB,    KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,       KC_U, \  
+        KC_CAPS,   KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,       KC_J, \
+        KC_LSHIFT, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,       KC_M, \
+        KC_LCTRL,  KC_LGUI, KC_LALT,                    KC_SPC );
 //        ,LAYER_1,
 
 /*
